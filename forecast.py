75765828cd5deb31d2a1dd6d6a33716d54146e15
James White <jody@jody-white.com>
2015-05-05 18:26:12 -0400

    Initial Commit

---
+++ forecast.py
@@ -0,0 +1,34 @@
+import forecastio
+import re
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
+
+f = open('api.txt', 'r')
+
+api_key = f.read()
+
+zip = input("Enter your zip: ")
+zip = str(zip)
+
+zipCode = re.compile(r"^\d{5}(-\d{4})?$")
+
+if zipCode.match(zip):
+    location = geolocator.geocode(zip)
+
+    forecast = forecastio.load_forecast(api_key, location.latitude, location.longitude)
+
+    current_temp = forecast.currently()
+
+    print "Current temp is:",current_temp.temperature
+    print "Currently it is:",current_temp.summary
+    print "Nearest storm is:",current_temp.nearestStormDistance,"miles."
+
+else:
+    print "The zip is not valid."
+
+
+byDay = forecast.daily()
+for dailyData in byDay.data:
+   print dailyData.time,dailyData.temperatureMax,dailyData.temperatureMin
+
